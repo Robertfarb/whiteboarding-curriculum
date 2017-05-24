0a12c635d71ed3fa3c69aae6197f6bcded1c0971
Andrew MacIver <maciver.andrew@gmail.com>
2017-05-23 17:01:02 -0700

    fix specs on topo sort and edit readme

--- algorithms/graphs/project/firstname_lastname/spec/topological_sort_spec.rb
+++ algorithms/graphs/project/firstname_lastname/spec/topological_sort_spec.rb
@@ -6,23 +6,43 @@ describe 'TopologicalSort' do
   let(:v2) { Vertex.new("Feed Markov") }
   let(:v3) { Vertex.new("Dry Markov") }
   let(:v4) { Vertex.new("Brush Markov") }
+  let(:v5) { Vertex.new("Cuddle Markov") }
+  let(:v6) { Vertex.new("Walk Markov") }
+  let(:v7) { Vertex.new("Teach Markov") }
+  let(:v8) { Vertex.new("Worship Markov") }
   let(:vertices) { [] }
 
   before(:each) do
-    vertices.push(v1, v2, v3, v4)
-    Edge.new(v1, v2)
-    Edge.new(v1, v3)
-    Edge.new(v2, v4)
-    Edge.new(v3, v4)
+    vertices.push(v1, v2, v3, v4, v5, v6, v7, v8)
+    # Edge.new(v1, v2)
+    # Edge.new(v1, v3)
+    # Edge.new(v2, v4)
+    # Edge.new(v3, v4)
+    # Edge.new(v2, v5)
+    # Edge.new(v4, v6)
+    # Edge.new(v5, v6)
+    # Edge.new(v6, v7)
+    # Edge.new(v7, v8)
   end
 
   it "sorts the vertices" do
     solutions = [
-      [v1, v2, v3, v4],
-      [v1, v3, v2, v4]
+      [v1, v2, v3, v4, v5, v6, v7, v8],
+      [v1, v2, v3, v5, v4, v6, v7, v8],
+      [v1, v3, v2, v4, v5, v6, v7, v8],
+      [v1, v3, v2, v5, v4, v6, v7, v8]
     ]
 
     20.times do
+      Edge.new(v1, v2)
+      Edge.new(v1, v3)
+      Edge.new(v2, v4)
+      Edge.new(v3, v4)
+      Edge.new(v2, v5)
+      Edge.new(v4, v6)
+      Edge.new(v5, v6)
+      Edge.new(v6, v7)
+      Edge.new(v7, v8)
       expect(solutions).to include(topological_sort(vertices.shuffle))
     end
   end
