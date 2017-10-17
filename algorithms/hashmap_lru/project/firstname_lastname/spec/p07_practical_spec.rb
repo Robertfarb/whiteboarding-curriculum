ebe532a7fa64ee34d9098e19598d3b7e646e04e0
Joshua Ling <joshling1919@gmail.com>
2017-10-17 12:35:29 -0700

    Add even-length word for palindrome test

--- algorithms/hashmap_lru/project/firstname_lastname/spec/p07_practical_spec.rb
+++ algorithms/hashmap_lru/project/firstname_lastname/spec/p07_practical_spec.rb
@@ -6,9 +6,11 @@ describe '#can_string_be_palindrome?' do
     result_1 = can_string_be_palindrome?("edified")
     result_2 = can_string_be_palindrome?("apple")
     result_3 = can_string_be_palindrome?("racecar")
+    result_4 = can_string_be_palindrome?("noon")
 
     expect(result_1).to be(true)
     expect(result_2).to be(false)
     expect(result_3).to be(true)
+    expect(result_4).to be(true)
   end
 end
